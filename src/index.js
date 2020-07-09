555c8ee7b149a7250463af0e6c66c5663d9a6238
Msn <nazar@mailcohen.com>
2020-07-09 22:10:02 +0500

    adding button component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-import React, { Component } from 'react';
+import {Button}  from './button';
 
-export default class DummyComponent extends Component {
-
-    render () {
-
-        return (
-            <div>I am a dummy react npm module</div>
-        )
-
-    }
-
-}
+export default [Button];
